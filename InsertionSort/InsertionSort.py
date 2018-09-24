77df219c1eb9f909f582cc32ef266294b0b0a39f
qmPeng <pengyuca@gmail.com>
2018-09-24 09:32:51 +0800

    Signed-off-by: qmPeng <pengyuca@gmail.com>

---
+++ InsertionSort/InsertionSort.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#insertion sort based by Peng
+
+def insertionSort(myList):
+    for i in range(1, len(myList)):
+        rightvalue = myList[i]
+        position = i
+
+        while position > 0 and myList[position-1] > rightvalue:
+            myList[i] = myList[position-1]
+            position = position -1
+
+        myList[position] = rightvalue
+
+
+
+arr = [64,25,93,37,66,38,24,75,26]
+insertionSort(arr)
+print(arr)
