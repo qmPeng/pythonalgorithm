86ac1b226fc30c685f92c412b137dc24dbeb15ea
Peng <pengyuca@gmail.com>
2018-10-12 08:06:51 +0800

    Signed-off-by: Peng <pengyuca@gmail.com>

---
+++ ListofLists/ListofLists.py
@@ -0,0 +1,51 @@
+def BinaryTree(r):
+    return [r, [], []]
+
+
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
+
+
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
+
+
+def getRootVal(root):
+    return root[0]
+
+
+def setRootVal(root,newVal):
+    root[0] = newVal
+
+
+def getLeftChild(root):
+    return root[1]
+
+
+def getRightChild(root):
+    return root[2]
+
+
+right = BinaryTree(3)
+insertLeft(right, 4)
+insertLeft(right, 5)
+insertRight(right, 6)
+insertRight(right, 7)
+left = getLeftChild(right)
+print(left)
+
+setRootVal(left, 9)
+print(right)
+insertLeft(left, 11)
+print(right)
+print(getRightChild(getRightChild(right)))
